a836f07f0bd64f074a0342d75780fa679d795001
Jan Rueger <jrueger@online.gibz.ch>
2019-06-08 19:04:58 +0200

    Added update and delete functionalities for product

---
+++ API/API/Features/v1/ProductArea/Update/ProductUpdateHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using CLERP.API.Infrastructure.Contexts;
+using CLERP.API.Infrastructure.Exceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CLERP.API.Features.v1.ProductArea.Update
+{
+    public class ProductUpdateHandler : IRequestHandler<ProductUpdateRequest, ProductResponse>
+    {
+        private readonly ClerpContext _context;
+        private readonly IMapper _mapper;
+
+        public ProductUpdateHandler(ClerpContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<ProductResponse> Handle(ProductUpdateRequest request, CancellationToken cancellationToken)
+        {
+            var product = await _context.Products.FindByGuidAsync(request.ProductGuid, cancellationToken);
+
+            if (product == null)
+            {
+                throw new BadRequestException(); // product to update couldn't be found => cant update product
+            }
+
+            product.Description = request.Description;
+            product.State = request.State;
+            product.CompartmentGuid = request.CompartmentGuid;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return _mapper.Map<ProductResponse>(product);
+        }
+    }
+}
---
+++ API/API/Features/v1/ProductArea/Update/ProductUpdateRequest.cs
@@ -0,0 +1,25 @@
+﻿using CLERP.API.Domain.Models.Abstract;
+using MediatR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CLERP.API.Features.v1.ProductArea.Update
+{
+    public class ProductUpdateRequest : IRequest<ProductResponse>
+    {
+        [JsonIgnore]
+        public Guid ProductGuid { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("state")]
+        public ProductState State { get; set; }
+
+        [JsonProperty("compartmentId")]
+        public Guid CompartmentGuid { get; set; }
+    }
+}
